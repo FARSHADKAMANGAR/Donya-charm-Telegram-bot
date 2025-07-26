c4f18738d333e092c3538a20a1946aacc1658f29
FARSHADKAMANGAR <fkamangar1370@gmail.com>
2025-07-26 23:00:57 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from telegram import Update
+from telegram.constants import ParseMode
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+
+TOKEN = "8479547692:AAGCDvqDSr3XvtYZ3PSXz7UnRHPTrWYn0JE"
+
+MESSAGE_TEXT = """*ربات در حال بروزرسانی است و در دسترس نیست . . . 🌜*
+*منتظر بمانید یا اگر مشکلی دارید ، با پشتیبان ربات در ارتباط باشید ♻️*
+
+*iD : @DONYA_CHARM_SUPPORT*"""
+
+async def reply_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message:
+        await update.message.reply_text(
+            MESSAGE_TEXT,
+            parse_mode=ParseMode.MARKDOWN
+        )
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.ALL, reply_message))
+
+    print("ربات راه‌اندازی شد و منتظر پیام‌هاست...")
+    app.run_polling()
